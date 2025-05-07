b00e8cc3ca89d635c49e3a83b1425872e2dc26cd
Mmsshakil <softhandshakil@gmail.com>
2025-05-08 00:27:02 +0600

    Dereferencing a pointer

--- pointers.c
+++ pointers.c
@@ -5,9 +5,14 @@ int main()
 
     printf("%p\n", &x); // here i print the address of 10
 
-    int *ptr;          // here i declare a pointer value
-    ptr = &x;          // here i put the address in the ptr variable
-    printf("%p", ptr); // here print the address
+    int *ptr;             // here i declare a pointer value
+    ptr = &x;             // here i put the address in the ptr variable
+    printf("%p\n", ptr);  // here print the address
+    printf("%d\n", *ptr); // here print the value - Dereferencing a pointer
+
+    *ptr = 20;          // here i changed the value of x is 20 but didn't change the address
+    printf("%d\n", x);  // it will print new value of x is 20
+    printf("%d", *ptr); // here Dereferencing a pointer means , it will print the value of ptr address
 
     return 0;
 }
