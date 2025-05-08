ba5decaf9b22b39a4869249efb8c63f9f8ed84c2
Mmsshakil <softhandshakil@gmail.com>
2025-05-08 16:58:43 +0600

    array with function basic

--- array_with_function.c
+++ array_with_function.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+
+void fun(int arr[])
+{
+    arr[1] = 200;
+}
+
 int main()
 {
+    int arr[5] = {1, 2, 3, 4, 5};
+    fun(arr); // for array in function it pass by reference, that's why arr[1] will be 200
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
